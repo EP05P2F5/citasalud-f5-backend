22d8d2fcc465350de74a91d36ea4a88f89913979
Quirama1108 <sebastian.castaneda1@udea.edu.co>
2025-10-21 21:22:49 -0500

    PipeLine Arquisoft

--- target/generated-sources/annotations/com/feature5/pqrs/mapper/UsuarioMapperImpl.java
+++ target/generated-sources/annotations/com/feature5/pqrs/mapper/UsuarioMapperImpl.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-10-20T23:53:37-0500",
+    date = "2025-10-21T21:20:27-0500",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 17.0.12 (Oracle Corporation)"
 )
 @Component
