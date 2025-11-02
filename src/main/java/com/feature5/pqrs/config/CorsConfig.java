b09aa388b67fcbe541b08d9031dcaac4b87ba295
John Edison Zapata Ramirez <67084916+John3814@users.noreply.github.com>
2025-11-02 18:25:36 -0500

    Create CorsConfig.java

---
+++ src/main/java/com/feature5/pqrs/config/CorsConfig.java
@@ -0,0 +1,10 @@
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .allowedHeaders("*");
+    }
+}
